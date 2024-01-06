eb378e4e93fb6a1aaa985561f8bab601b609ae5d
hugosmoreira <hugo.n4@gmail.com>
2024-01-06 13:41:17 -0800

    update

--- events-app/app/(root)/page.tsx
+++ events-app/app/(root)/page.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
+      <h1 className='...'>EventsBJJ</h1>
       <Button variant="destructive" className='button'>Click me</Button>
     </main>
   )
