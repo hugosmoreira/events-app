73b1528f3889535c63241f1e1a4113503c4bd13a
hugosmoreira <hugo.n4@gmail.com>
2024-01-16 11:25:51 -0800

    update

--- events-app/components/shared/Header.tsx
+++ events-app/components/shared/Header.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='wrapper'>
+      <div className='wrapper'>
+        <Link href='/' className='w-36'>
+          <Image src='/assets/images/logo.svg' width={128} height={38} alt='Event Logo'></Image>
+        </Link>
+
+        <div className='flex w-32 justify-end gap-3'>
+
+        </div>
+      </div>
+    </header>
   )
 }
 
